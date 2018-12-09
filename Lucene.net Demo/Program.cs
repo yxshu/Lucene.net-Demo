b42e2a7bfd5a9a9419fde4f868c5e6816748fe65
yxshu <yxshu@qq.com>
2018-12-09 23:51:09 +0800

    00
    000

--- Lucene.net Demo/Program.cs
+++ Lucene.net Demo/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,19 +22,13 @@ namespace Lucene.net_Demo
 
             ///利用反射和泛型将Datatable中的数据转换成对象对放入list中
             List<QuestionModel> list = Utility.DataTableToModel<QuestionModel>(dt, typeof(QuestionModel));
-            Console.WriteLine(list.Count);
+            Console.WriteLine("共获取内容{0}条。", list.Count);
+
             ///利用Lucene.NET生成索引文件Index
-            Console.WriteLine("开始索引----------------");
-            SearchHelper searchhelper = SearchHelper.GetInstance();
-            searchhelper.CustomerStart();
-            foreach (QuestionModel q in list)
-            {
-                searchhelper.AddArticle(q);
-            }
-
+            Console.WriteLine("----------------开始索引----------------");
             ///利用Lucene.NET查询索引文件并高亮显示以及按相关度排序
-
-
+            SearchHelper searchhelper = SearchHelper.GetInstance();
+            searchhelper.CreatIndexsByDescription(new List<object>(list));
             Console.ReadLine();
         }
     }
--- Lucene.net Demo/SearchHelper.cs
+++ Lucene.net Demo/SearchHelper.cs
@@ -1,342 +1,153 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Configuration;
-using System.Threading;
-using Lucene.Net.Store;
-using System.IO;
+﻿using Lucene.Net.Analysis;
 using Lucene.Net.Index;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Lucene.Net.Store;
 using Lucene.Net.Documents;
-using Lucene.Net.Search;
-using Lucene.Net.QueryParsers;
-using PanGu;
-using PanGu.HighLight;
-using Lucene.Net.Analysis;
-using PanGu.Lucene.Analyzer;
+using System.Reflection;
+using System.ComponentModel;
 
 namespace Lucene.net_Demo
 {
     public class SearchHelper
     {
-
-        #region 创建单例
-        // 定义一个静态变量来保存类的实例
+        // 定义一个静态变量来保存类的实例,使用单例模式
         private static SearchHelper uniqueInstance;
 
-        // 定义一个标识确保线程同步
-        private static readonly object locker = new object();
+        private SearchHelper() { }// 定义私有构造函数，使外界不能创建该类实例
 
-
-        // 定义私有构造函数，使外界不能创建该类实例
-        private SearchHelper()
-        {
-            CustomerStart();
-        }
-
-        /// <summary>
-        /// 定义公有方法提供一个全局访问点,同时你也可以定义公有属性来提供全局访问点
-        /// </summary>
-        /// <returns></returns>
         public static SearchHelper GetInstance()
         {
-            // 当第一个线程运行到这里时，此时会对locker对象 "加锁"，
-            // 当第二个线程运行该方法时，首先检测到locker对象为"加锁"状态，该线程就会挂起等待第一个线程解锁
-            // lock语句运行完之后（即线程运行完之后）会对该对象"解锁"
-            lock (locker)
+            if (uniqueInstance == null)
             {
-                // 如果类的实例不存在则创建，否则直接返回
-                if (uniqueInstance == null)
-                {
-                    uniqueInstance = new SearchHelper();
-                }
+                uniqueInstance = new SearchHelper();
             }
-
             return uniqueInstance;
         }
-
-        #endregion
-
-        private Queue<IndexJob> jobs = new Queue<IndexJob>();       //任务队列,保存生产出来的任务和消费者使用,不使用list避免移除时数据混乱问题
-
         /// <summary>
-        /// 任务类,包括任务的Id ,操作的类型
+        /// 打开索引目录
+        /// 索引的地址写在配置文件的appsettings中，以“IndexDir”命名
         /// </summary>
-        class IndexJob
+        /// <returns>返回打开的目录</returns>
+        private FSDirectory CreateFSDirectory()
         {
-            public int Id { get; set; }//写入索引的数据
-            public JobType JobType { get; set; }//处理数据的方式
-            public QuestionModel question { get; set; }//加处理的数据的类型
-        }
-        /// <summary>
-        /// 枚举,操作类型是增加还是删除
-        /// </summary>
-        enum JobType { Add, Remove }
-
-        #region 任务索引
-        /// <summary>
-        /// 启动消费者线程
-        /// </summary>
-        public void CustomerStart()
-        {
-
-           // PanGu.Segment.Init(PanGuPath);
-
-            Thread threadIndex = new Thread(IndexOn);
-            threadIndex.IsBackground = true;
-            threadIndex.Start();
-        }
-
-        /// <summary>
-        /// 索引任务线程
-        /// </summary>
-        private void IndexOn()
-        {
-            Console.WriteLine("索引任务线程启动");
-            while (true)
+            string IndexDir = System.Configuration.ConfigurationManager.AppSettings["IndexDir"];
+            if (!System.IO.Directory.Exists(IndexDir))
             {
-                if (jobs.Count <= 0)
+                System.IO.Directory.CreateDirectory(IndexDir);
+            }
+            FSDirectory directory = FSDirectory.Open(new DirectoryInfo(IndexDir), new NativeFSLockFactory());
+            Console.WriteLine(string.Format("打开索引目录{0}成功。", IndexDir));
+            return directory;
+        }
+
+        /// <summary>
+        /// 生成索引创建器IndexWriter
+        /// </summary>
+        /// <returns>indexwriter</returns>
+        private IndexWriter CreateWriter()
+        {
+            FSDirectory directory = CreateFSDirectory();
+            IndexWriter writer = null;
+            //如果索引目录被锁定（比如索引过程中程序异常退出），则首先解锁
+            if (IndexWriter.IsLocked(directory))
+            {
+                IndexWriter.Unlock(directory);
+            }
+            writer = new IndexWriter(directory, new PanGuAnalyzer(), true, Lucene.Net.Index.IndexWriter.MaxFieldLength.LIMITED);//生成索引写手
+            Console.WriteLine(string.Format("生成IndexWriter:{0}成功。", writer.Directory.ToString()));
+            return writer;
+        }
+
+        /// <summary>
+        ///写入索引 ,主要通过model属性上的Description中标注的store 和index进行识别和索引
+        ///调用完成以后，记得关闭indexwriter
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="obj"></param>
+        private Boolean CreatIndexByDescription(IndexWriter indexwriter, object obj)
+        {
+            bool success = false;
+            Document document = new Document();//一条记录
+            Type type = obj.GetType();
+            PropertyInfo[] propertyinfos = type.GetProperties();
+            try
+            {
+                foreach (PropertyInfo propertyinfo in propertyinfos)
                 {
-                    Thread.Sleep(5 * 1000);
-                    continue;
-                }
-                //创建索引目录
-                if (!System.IO.Directory.Exists(IndexDic))
-                {
-                    System.IO.Directory.CreateDirectory(IndexDic);
-                }
-                FSDirectory directory = FSDirectory.Open(new DirectoryInfo(IndexDic), new NativeFSLockFactory());
-                bool isUpdate = IndexReader.IndexExists(directory);
-                Console.WriteLine("索引库存在状态" + isUpdate);
-                if (isUpdate)
-                {
-                    //如果索引目录被锁定（比如索引过程中程序异常退出），则首先解锁
-                    if (IndexWriter.IsLocked(directory))
+                    Field.Store store = Field.Store.NO;
+                    Field.Index index = Field.Index.NO;
+                    DescriptionAttribute attr = (DescriptionAttribute)propertyinfo.GetCustomAttribute(typeof(DescriptionAttribute));
+                    if (attr == null) continue;
+                    string[] str = attr.Description.ToString().Split();
+                    foreach (string s in str)
                     {
-                        Console.WriteLine("开始解锁索引库");
-                        IndexWriter.Unlock(directory);
-                        Console.WriteLine("解锁索引库完成");
+                        if (string.Equals(s.Trim().ToLower(), "store"))
+                        {
+                            store = Field.Store.YES;
+                        }
+                        else if (string.Equals(s.Trim().ToLower(), "index"))
+                        {
+                            index = Field.Index.ANALYZED;
+                        }
+                    }
+                    if (store != Field.Store.NO || index != Field.Index.NO)
+                    {
+                        document.Add(new Field(propertyinfo.Name, propertyinfo.GetValue(obj).ToString(), store, index));
                     }
                 }
-                IndexWriter writer = new IndexWriter(directory, new PanGuAnalyzer(), !isUpdate, Lucene.Net.Index.IndexWriter.MaxFieldLength.UNLIMITED);//写索引记录器
-                ProcessJobs(writer);//将job queue中的任务全部写入索引
-                writer.Close();
-                directory.Dispose();//不要忘了Close，否则索引结果搜不到
-                Console.WriteLine("全部索引完毕");
+                if (document.GetFields().Count > 0)
+                {
+                    indexwriter.AddDocument(document);
+                    success = true;
+                    Console.WriteLine("{0}创建索引成功。", obj.ToString());
+                }
+                return success;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
             }
         }
+
         /// <summary>
-        /// 写入索引
+        /// 创建索引
         /// </summary>
-        /// <param name="writer"></param>
-        private void ProcessJobs(IndexWriter writer)
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public Boolean CreatIndexsByDescription(List<object> list)
         {
-            while (jobs.Count != 0)
+            bool success = true;
+            IndexWriter indexwriter = CreateWriter();
+            try
             {
-                IndexJob job = jobs.Dequeue();//取得最上面的工作任务
-                writer.DeleteDocuments(new Term("number", job.Id.ToString()));//删除索引，job.id为标识，一般的原则是先移除再添加
-                //如果“添加文章”任务再添加，
-                if (job.JobType == JobType.Add)
+                foreach (object obj in list)
                 {
-
-                    //BLL.article bll = new BLL.article();
-                    //if (bll == null)//有可能刚添加就被删除了
-                    //{
-                    //    continue;
-                    //}
-                    //Model.article art = bll.GetArticleModel(job.Id);
-                    QuestionModel question = job.question;
-                    //string channel_id = art.channel_id.ToString();
-                    //string title = art.title;
-                    //DateTime time = art.add_time;
-                    //string content = Utils.DropHTML(art.content.ToString());
-                    //string Addtime = art.add_time.ToString("yyyy-MM-dd");
-
-                    Document document = new Document();
-                    document.Add(new Field("ALLID",question.Allid.ToString(),Field.Store.YES,Field.Index.NOT_ANALYZED));
-                    document.Add(new Field("TITLE",question.Title,Field.Store.NO,Field.Index.ANALYZED));
-                    document.Add(new Field("CHOOSEA",question.Choosea,Field.Store.NO,Field.Index.ANALYZED));
-                    document.Add(new Field("CHOOSEB", question.Chooseb, Field.Store.NO, Field.Index.ANALYZED));
-                    document.Add(new Field("CHOOSEC", question.Choosec, Field.Store.NO, Field.Index.ANALYZED));
-                    document.Add(new Field("CHOOSED", question.Choosed, Field.Store.NO, Field.Index.ANALYZED));
-                    document.Add(new Field("ANSWER", question.Answer, Field.Store.NO, Field.Index.ANALYZED));
-                    ////只有对需要全文检索的字段才ANALYZED
-                    //document.Add(new Field("number", job.Id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    //document.Add(new Field("title", title, Field.Store.YES, Field.Index.ANALYZED, Lucene.Net.Documents.Field.TermVector.WITH_POSITIONS_OFFSETS));
-                    //document.Add(new Field("channel_id", channel_id, Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    //document.Add(new Field("Addtime", Addtime, Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    //document.Add(new Field("content", content, Field.Store.YES, Field.Index.ANALYZED, Lucene.Net.Documents.Field.TermVector.WITH_POSITIONS_OFFSETS));
-                    writer.AddDocument(document);
-                    Console.WriteLine("索引" + job.Id + "完毕");
+                    if (!CreatIndexByDescription(indexwriter, obj))
+                    {
+                        success = false;
+                        break;
+                    }
                 }
             }
-        } 
-        #endregion
-
-        #region 任务添加
-        public void AddArticle(QuestionModel question)
-        {
-            IndexJob job = new IndexJob();
-            job.Id = question.Allid;
-            job.JobType = JobType.Add;
-            job.question = question;
-            Console.WriteLine(question.Allid + "加入任务列表");
-            jobs.Enqueue(job);//把任务加入商品库
-        }
-
-        public void RemoveArticle(QuestionModel question)
-        {
-            IndexJob job = new IndexJob();
-            job.JobType = JobType.Remove;
-            job.Id =question.Allid ;
-            job.question = question;
-            Console.WriteLine(question.Allid + "加入删除任务列表");
-            jobs.Enqueue(job);//把任务加入商品库
-        }
-        #endregion
-
-        //#region 从索引搜索结果
-        ///// <summary>
-        ///// 从索引搜索结果
-        ///// </summary>
-        //public List<Model.article> SearchIndex(string Words, int PageSize, int PageIndex, out int _totalcount)
-        //{
-        //    _totalcount = 0;
-        //    Dictionary<string, string> dic = new Dictionary<string, string>();
-        //    BooleanQuery bQuery = new BooleanQuery();
-        //    string title = string.Empty;
-        //    string content = string.Empty;
-        //    title = GetKeyWordsSplitBySpace(Words);
-        //    QueryParser parse = new QueryParser(Lucene.Net.Util.Version.LUCENE_29, "title", new PanGuAnalyzer());
-        //    Query query = parse.Parse(title);
-        //    parse.SetDefaultOperator(QueryParser.Operator.AND);
-        //    bQuery.Add(query, BooleanClause.Occur.SHOULD);
-        //    dic.Add("title", Words);
-
-        //    content = GetKeyWordsSplitBySpace(Words);
-        //    QueryParser parseC = new QueryParser(Lucene.Net.Util.Version.LUCENE_29, "content", new PanGuAnalyzer());
-        //    Query queryC = parseC.Parse(content);
-        //    parseC.SetDefaultOperator(QueryParser.Operator.AND);
-        //    bQuery.Add(queryC, BooleanClause.Occur.SHOULD);
-        //    dic.Add("content", Words);
-        //    if (bQuery != null && bQuery.GetClauses().Length > 0)
-        //    {
-        //        return GetSearchResult(bQuery, dic, PageSize, PageIndex, out _totalcount);
-        //    }
-        //    return null;
-        //}
-        ///// <summary>
-        ///// 获取
-        ///// </summary>
-        ///// <param name="bQuery"></param>
-        //private List<Model.article> GetSearchResult(BooleanQuery bQuery, Dictionary<string, string> dicKeywords, int PageSize, int PageIndex, out int totalCount)
-        //{
-        //    List<Model.article> list = new List<Model.article>();
-        //    FSDirectory directory = FSDirectory.Open(new DirectoryInfo(IndexDic), new NoLockFactory());
-        //    IndexReader reader = IndexReader.Open(directory, true);
-        //    IndexSearcher searcher = new IndexSearcher(reader);
-        //    TopScoreDocCollector collector = TopScoreDocCollector.create(1000, true);
-        //    Sort sort = new Sort(new SortField("Addtime", SortField.DOC, true));
-        //    searcher.Search(bQuery, null, collector);
-        //    totalCount = collector.GetTotalHits();//返回总条数
-        //    TopDocs docs = searcher.Search(bQuery, (Filter)null, PageSize * PageIndex, sort);
-        //    if (docs != null && docs.totalHits > 0)
-        //    {
-        //        for (int i = 0; i < docs.totalHits; i++)
-        //        {
-        //            if (i >= (PageIndex - 1) * PageSize && i < PageIndex * PageSize)
-        //            {
-        //                Document doc = searcher.Doc(docs.scoreDocs[i].doc);
-        //                Model.article model = new Model.article()
-        //                {
-        //                    id = int.Parse(doc.Get("number").ToString()),
-        //                    title = doc.Get("title").ToString(),
-        //                    content = doc.Get("content").ToString(),
-        //                    add_time = DateTime.Parse(doc.Get("Addtime").ToString()),
-        //                    channel_id = int.Parse(doc.Get("channel_id").ToString())
-        //                };
-        //                list.Add(SetHighlighter(dicKeywords, model));
-        //            }
-        //        }
-        //    }
-        //    return list;
-        //}
-        ///// <summary>
-        ///// 设置关键字高亮
-        ///// </summary>
-        ///// <param name="dicKeywords">关键字列表</param>
-        ///// <param name="model">返回的数据模型</param>
-        ///// <returns></returns>
-        //private Model.article SetHighlighter(Dictionary<string, string> dicKeywords, Model.article model)
-        //{
-        //    SimpleHTMLFormatter simpleHTMLFormatter = new PanGu.HighLight.SimpleHTMLFormatter("<font color=\"red\">", "</font>");
-        //    Highlighter highlighter = new PanGu.HighLight.Highlighter(simpleHTMLFormatter, new Segment());
-        //    highlighter.FragmentSize = 250;
-        //    string strTitle = string.Empty;
-        //    string strContent = string.Empty;
-        //    dicKeywords.TryGetValue("title", out strTitle);
-        //    dicKeywords.TryGetValue("content", out strContent);
-        //    if (!string.IsNullOrEmpty(strTitle))
-        //    {
-        //        string title = model.title;
-        //        model.title = highlighter.GetBestFragment(strTitle, model.title);
-        //        if (string.IsNullOrEmpty(model.title))
-        //        {
-        //            model.title = title;
-        //        }
-        //    }
-        //    if (!string.IsNullOrEmpty(strContent))
-        //    {
-        //        string content = model.content;
-        //        model.content = highlighter.GetBestFragment(strContent, model.content);
-        //        if (string.IsNullOrEmpty(model.content))
-        //        {
-        //            model.content = content;
-        //        }
-        //    }
-        //    return model;
-        //}
-        ///// <summary>
-        ///// 处理关键字为索引格式
-        ///// </summary>
-        ///// <param name="keywords"></param>
-        ///// <returns></returns>
-        //private string GetKeyWordsSplitBySpace(string keywords)
-        //{
-        //    PanGuTokenizer ktTokenizer = new PanGuTokenizer();
-        //    StringBuilder result = new StringBuilder();
-        //    ICollection<WordInfo> words = ktTokenizer.SegmentToWordInfos(keywords);
-        //    foreach (WordInfo word in words)
-        //    {
-        //        if (word == null)
-        //        {
-        //            continue;
-        //        }
-        //        result.AppendFormat("{0}^{1}.0 ", word.Word, (int)Math.Pow(3, word.Rank));
-        //    }
-        //    return result.ToString().Trim();
-        //} 
-        //#endregion
-
-
-        /// <summary>
-        /// 索引存放目录
-        /// </summary>
-        protected string IndexDic
-        {
-            get
+            catch (Exception e)
             {
-                return System.Configuration.ConfigurationManager.AppSettings["IndexDic"];
+                Console.WriteLine("创建索引失败，原因：{0}", e.Message);
+                throw e;
+
             }
-        }
-        /// <summary>
-        /// 盘古分词配置目录
-        /// </summary>
-        protected string PanGuPath
-        {
-            get
+            finally
             {
-                return System.Configuration.ConfigurationManager.AppSettings["PanGuPath"];
+                indexwriter.Optimize();
+                indexwriter.Close();
             }
+            Console.WriteLine("成功创建索引{0}条。", list.Count);
+            return success;
         }
     }
 }
